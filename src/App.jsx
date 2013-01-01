7994592dea99150147e40b4da4eafdf4708788b1
Luis Tormes <luisangeltormes@gmail.coom>
2013-01-01 01:37:41 -0430

    moifique los iconos el asie y le agrege alguno estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Layout from "./components/Layout/Layout";
+import HomeScreen from "./pages/home/HomeScreen";
+import MovieScreen from "./pages/movie/MovieScreen";
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={HomeScreen()} />
+          <Route path="/movie" element={MovieScreen()} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
