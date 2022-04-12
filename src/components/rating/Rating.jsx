26c765d9ad6026216546da028d9af832ede025c0
Luis Tormes <luisangeltormes@gmail.coom>
2022-04-12 09:45:33 -0400

    updated

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -2,25 +2,21 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import Rating from "react-star-rating-lite";
 import "react-circular-progressbar/dist/styles.css";
 
-import './rating.css';
+import "./rating.css";
 
 const RatingMovie = ({ percentage, vote_count, star_rating }) => {
-  console.log('star_rating', star_rating)
-
   return (
     <div className="rating">
-      <div className="circular-bar"  style={{ width: 50, height: 50 }}>
+      <div className="circular-bar" style={{ width: 50, height: 50 }}>
         <CircularProgressbar value={percentage} text={`${percentage}%`} />
       </div>
       <div>
-      <div className="stars">
-
-        
-      { star_rating !== undefined && <Rating value={`${Math.floor(star_rating)}`} readonly />}
-
-      </div>
+        <div className="stars">
+          {star_rating !== undefined && (
+            <Rating value={`${star_rating}`} readonly />
+          )}
+        </div>
         <span>- votes: {vote_count} -</span>
-      
       </div>
     </div>
   );
