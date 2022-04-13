9e1a88e58418f31dcfbadc5c93a3d4fc2d761fbb
Luis Tormes <luisangeltormes@gmail.coom>
2022-04-13 11:39:26 -0400

    Guarde el id de la movie selecionada para evitar hacer el fetch de nuevo, y mostrar el trailer mas rapido

--- src/context/contextProvider.jsx
+++ src/context/contextProvider.jsx
@@ -6,9 +6,21 @@ export const appContext = createContext();
 const ContextProvider = ({ children }) => {
   const [modalIsOpen, setModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [currentMovieId, setCurrentMovieId] = useState(null);
   const [trailerId, setTrailerId] = useState({});
 
   function handleSeeTrailer(movieId, media_type) {
+    if (movieId === currentMovieId) {
+      console.log("las otras veces ");
+
+      setModalOpen(true);
+      return;
+    }
+
+    console.log("enatra por primera vez ");
+
+    setCurrentMovieId(movieId);
+
     const api_key = import.meta.env.VITE_API_KEY;
     setModalOpen(true);
     setLoading(true);
