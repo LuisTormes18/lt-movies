56492c2ab6ebca55071f43394d693a22a9f38920
Luis Tormes <luisangeltormes@gmail.coom>
2022-04-13 11:58:14 -0400

    ahregue lazy loadung a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
+import {lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import HomeScreen from "./pages/home/HomeScreen";
-import MovieScreen from "./pages/movie/MovieScreen";
-import SearchScreen from "./pages/search/SearchScreen";
 import ContextProvider from "./context/contextProvider";
+
+const HomeScreen = lazy(() => import("./pages/home/HomeScreen")) ;
+const MovieScreen =  lazy(() => import("./pages/movie/MovieScreen"));
+const SearchScreen = lazy(() => import("./pages/search/SearchScreen"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <ContextProvider>
         <Layout>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/movie/:media" element={<MovieScreen />} />
-            <Route path="/search" element={<SearchScreen />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/movie/:media" element={<MovieScreen />} />
+              <Route path="/search" element={<SearchScreen />} />
+            </Routes>
+          </Suspense>
         </Layout>
       </ContextProvider>
     </BrowserRouter>
